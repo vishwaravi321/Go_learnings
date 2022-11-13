ef1a701a17a6eda675f71b7ea899cac9bca86141
vishwa <vishwaravi321@gmail.com>
2022-11-13 15:29:15 +0530

    Again

--- Lectures/function.go
+++ Lectures/function.go
@@ -1,6 +1,27 @@
+package main
 
-func sum(lhs,rhs int) int {
+import "fmt"
+
+func double(x int) int{
+	return x+x
+}
+
+func add(lhs,rhs int) int{
 	return lhs + rhs
 }
 
-result := sum(2, 2)
+func greet() {
+	fmt.Println("Hello !! from greet function")
+}
+
+func main() {
+	greet()
+	dozen := double(6)
+	fmt.Println("A dozen is",dozen)
+
+	bakersDozen := add(dozen,1)
+	fmt.Println("A Baker's dozen is",bakersDozen)
+
+	anotherBakersDozen := add(double(6),1)
+	fmt.Println("Another Baker's dozeń is ",anotherBakersDozen)
+}
---
+++ function.go
@@ -0,0 +1,6 @@
+
+func sum(lhs,rhs int) int {
+	return lhs + rhs
+}
+
+result := sum(2, 2)
